aa4b88c844e9f4f9b6389a224a1ff139e5da1068
avokado34 <37953200+avokado34@users.noreply.github.com>
2020-05-11 10:50:55 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -112,6 +112,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
+        centerTitle: true,
         title: Text(widget.title),
       ),
       body: Center (
